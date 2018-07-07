fdbf5b3fbf810ca12262439181092664b95fb140
Daniele Caputo <caputo.daniele.n1@gmail.com>
2018-07-07 18:41:07 +0200

    Added MAPBOX API

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoieHd4bnVtYmVyMXh3eCIsImEiOiJjampibWczZzcxdHBwM3B0ZTBqeW9rbTlzIn0.LTKEnoh7Zzc5o-C0skji4A',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
